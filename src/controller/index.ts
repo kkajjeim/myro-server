94454e52f14d6b287aba56f0dd1b68d3cd00223c
oowgnoj <mangtaob@gmail.com>
2020-10-24 18:56:36 +0900

    arrage module import ES6 to commonJS

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import * as express from 'express';
 import userRouter from './user';
 
 const router = express.Router();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import "dotenv/config";
-import express from "express";
-import bodyParser from "body-parser";
+import * as express from 'express'
+import * as bodyParser from "body-parser";
 import {createConnection, getConnection} from "typeorm";
 import {errorHanlder} from "./middleware/error";
 import router from "./controller";
@@ -11,8 +11,8 @@ const nodeEnv = process.env.NODE_ENV || 'development';
 
 const main = async () => {
     await createConnection();
-
     const app = express();
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(router);
